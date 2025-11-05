c7b39cf4318bb6875140b1c2266aa32169e84797
kulkarni-karthik <karthik.k@oppty.in>
2025-11-05 17:57:48 +0530

    Day 21 task completed

---
+++ Day 21/task.js
@@ -0,0 +1,28 @@
+let name="karthik";
+var age = 22;
+
+let text = "Java Script" //String
+let num = 26; //number
+let isActive = true;  //Boolean
+let x; //Undfined
+let a = null //Null
+let color = ["orange", "blue", "voilet"] //Array
+
+let car = {
+    make: "Landrover",
+    model: "G-Defender",  //Object
+    year: 2024
+};
+
+//BIGINT
+let k = 1111234567898765432234567n;
+let m = BigInt(123452345698765432234567);
+
+//Date Object
+const date = new Date("04-11-2025")
+
+//Array Object
+const bike = ["Honda", "TVS", "Triumph"]
+
+document.getElementById("js").innerHTML = bike;
+console.log(car)
